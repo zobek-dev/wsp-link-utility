2f78e29b61f38464ce6396695e6d61cc652ea583
zobek-dev <zobek.dev@gmail.com>
2022-10-18 09:00:43 -0300

    Fixing white spaces in number

--- linkwsp.py
+++ linkwsp.py
@@ -2,7 +2,7 @@ def main():
   num = input("ingresa numero de wsp (de esta forma 569xxxxxxxx): ")
   msg = input("ingresa el mensaje: ")
 
-  url="https://wa.me/" + num.strip() + "/?text=" +(msg.strip().replace(" ","%20"))
+  url="https://wa.me/" + num.strip().replace(" ", "") + "/?text=" +(msg.strip().replace(" ","%20"))
 
   print('El link para el wsp es: "' + url + '"')
 
