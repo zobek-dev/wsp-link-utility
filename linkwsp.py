03e6795ef3d617213847b82f01998d959701a5df
zobek-dev <zobek.dev@gmail.com>
2022-11-10 08:30:02 -0300

    arreglo de otras cosas

--- linkwsp.py
+++ linkwsp.py
@@ -1,8 +1,8 @@
 def main():
   num = input("ingresa numero de wsp (de esta forma 569xxxxxxxx): ")
-  msg = input("ingresa el mensaje: ")
+  msg = input("ingresa el mensaje: ") 
 
-  url="https://wa.me/" + num.strip().replace(" ", "") + "/?text=" +(msg.strip().replace(" ","%20"))
+  url="https://wa.me/" + num.strip().replace(" ", "") + "/?text=" +(msg.strip().replace(" ","%20").replace("!", "%21").replace(".", "%2E"))
 
   print('El link para el wsp es: "' + url + '"')
 
