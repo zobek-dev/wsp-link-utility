62c4c090a750f7365e5ab39b5dce61e3d5e6e639
zobek-dev <zobek.dev@gmail.com>
2023-02-21 15:56:31 -0300

    Remplazo de signo + por vacio

--- linkwsp.py
+++ linkwsp.py
@@ -2,7 +2,7 @@ def main():
   num = input("ingresa numero de wsp (de esta forma 569xxxxxxxx): ")
   msg = input("ingresa el mensaje: ") 
 
-  url="https://wa.me/" + num.strip().replace(" ", "") + "/?text=" +(msg.strip().replace(" ","%20").replace("!", "%21").replace(".", "%2E"))
+  url="https://wa.me/" + num.strip().replace(" ", "").replace("+", "") + "/?text=" +(msg.strip().replace(" ","%20").replace("!", "%21").replace(".", "%2E").replace(",", "%2C").replace("#", "%23"))
 
   print('El link para el wsp es: "' + url + '"')
 
